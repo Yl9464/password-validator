4e421bc2aaa3b9cedb49d98dd930c046bf31abea
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-11-10 11:09:26 -0500

    trying for loop

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-const validatePassword = () => {
 
-}
+//const length = []
+//const upperCase = [length >= 1]
+//const lowerCase = [length >= 1]
+//const number = [length >= 1]
+//const character = [length >= 1]
 
-module.exports = validatePassword
+const validatePassword = (number, upperCase, lowerCase, length, character) => {
+   let valid = []
+//infput: 5 vairables
+    for (i = 0; i < arr.length; i++) {
+
+   if (valid = (arr.length <= 8)){
+       return false
+   }
+
+    if (valid = (!lowerCase && !upperCase) || (!lowerCase && upperCase) || (lowerCase && !upperCase)){
+        return false
+    }
+
+    if(valid = (number <= 1)) {
+        return fales
+    }
+
+    if (valid = (character <= 1)) {
+        return false
+    }
+
+    else {
+        return true
+    }
+
+    module.exports = validatePassword
