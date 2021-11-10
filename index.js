21e26634cc9f9be1114a48b6dbba221c73bff6e0
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-11-10 11:16:53 -0500

    false statements passing

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
 
-//const length = []
-//const upperCase = [length >= 1]
-//const lowerCase = [length >= 1]
-//const number = [length >= 1]
-//const character = [length >= 1]
+// const length = []
+// const upperCase = [length >= 1]
+// const lowerCase = [length >= 1]
+// const number = [length >= 1]
+// const character = [length >= 1]
 
-const validatePassword = (number, upperCase, lowerCase, length, character) => {
-   let valid = []
-//infput: 5 vairables
-    for (i = 0; i < arr.length; i++) {
+const validatePassword = (length) => {
+  const password = ('')
 
-   if (valid = (arr.length <= 8)){
-       return false
-   }
+  if (password.length >= 8) {
+    return true
+  }
+  else {
+    return false
+  }
+}
 
-    if (valid = (!lowerCase && !upperCase) || (!lowerCase && upperCase) || (lowerCase && !upperCase)){
-        return false
-    }
 
-    if(valid = (number <= 1)) {
-        return fales
-    }
-
-    if (valid = (character <= 1)) {
-        return false
-    }
-
-    else {
-        return true
-    }
-
-    module.exports = validatePassword
+module.exports = validatePassword
