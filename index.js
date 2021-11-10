0baf7c5c88383224309dd8b4c1be465b04a65465
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-11-10 11:41:44 -0500

    all requirements failing

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 
-// const length = []
+
 // const upperCase = [length >= 1]
 // const lowerCase = [length >= 1]
 // const number = [length >= 1]
 // const character = [length >= 1]
 
-const validatePassword = (length) => {
-  const password = ('')
+const validatePassword = (password) => {
+  var upperCase = /[A-Z]/g
+  var lowerCase = /[a-z]/g
+  var characters = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/
 
-  if (password.length >= 8) {
+  if (password < upperCase) {
+    return true
+  }
+  if (password < lowerCase) {
+    return true
+  }
+  if (password.length <= 8) {
+    return false
+  }
+  if (password < characters) {
     return true
   }
   else {
     return false
   }
 }
-
 
 module.exports = validatePassword
